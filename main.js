d661a82ebfa0e9265fc7021e6ca14c2f4f4a2640
Jeff Harper <jeffharperartist@gmail.com>
2024-05-19 09:26:45 -0700

    Added a bunch of stuff

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
-
 'use strict';
 
 /* ---------------- Animation observer ---------------- */
@@ -35,6 +10,7 @@ const animationObserver = new IntersectionObserver(entries => {
     }
   });
 }, { threshold: 0.4 });
+
 
 const animatedElements = document.querySelectorAll('[class*="animate"]');
 animatedElements.forEach((element) => animationObserver.observe(element));
